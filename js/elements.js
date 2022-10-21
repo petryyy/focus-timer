0f77f103f42a1134750a13b2c00c4a0b34be5462
João Pedro Petry <jp.petrydev@gmail.com>
2022-10-21 18:40:42 -0300

    Add files via upload

---
+++ js/elements.js
@@ -0,0 +1,19 @@
+const buttonPause = document.querySelector('.pause')
+const buttonPlay = document.querySelector('.play')
+const buttonStop = document.querySelector('.stop')
+const buttonSet = document.querySelector('.set')
+const buttonSoundOn = document.querySelector('.sound-on')
+const buttonSoundOff = document.querySelector('.sound-off')
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+
+export {
+  buttonPause,
+  buttonPlay,
+  buttonSet,
+  buttonSoundOff,
+  buttonSoundOn,
+  buttonStop,
+  minutesDisplay,
+  secondsDisplay,
+}
